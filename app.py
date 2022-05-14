b50ea92f81e62359e768d2e882557e1f7630f02d
sanjana shivanand <sanjanashivanand@sanjanas-MacBook-Air.local>
2022-05-15 02:49:50 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 15 02:24:49 2022
+
+@author: sanjanashivanand
+"""
+
+import flask 
+from flask import request
+import pandas as pd
+
+df = pd.read_csv('Dataset.csv')
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    id = request.args['id']
+    try:
+        ele = df.index[df['id'] == id]
+        return df.iloc[ele]
+    except KeyError:
+        return "Not in the Database"
